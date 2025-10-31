1d944953fce8c22929b5e38353f51210fc759a83
Ardavan Alaei Fard <ardavanalaei@gmail.com>
2025-10-30 23:51:57 -0400

    Added gazebo sim to setup

--- run.sh
+++ run.sh
@@ -5,11 +5,20 @@ set -e
 export HOST_UID=$(id -u)
 export HOST_GID=$(id -g)
 
+# Default: simulation off
+SIM_MODE="OFF"
+if [[ "$1" == "--sim" ]]; then
+  SIM_MODE="ON"
+fi
+
 docker compose build \
   --build-arg UID=$HOST_UID \
-  --build-arg GID=$HOST_GID
+  --build-arg GID=$HOST_GID \
+  --build-arg SIM_MODE=$SIM_MODE
 
 docker compose run --rm \
   -e UID=$HOST_UID \
   -e GID=$HOST_GID \
+  -e SIM_MODE=$SIM_MODE \
+  --name snoopdogg_container \
   ros2 bash
