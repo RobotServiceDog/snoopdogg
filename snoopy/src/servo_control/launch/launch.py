58fdf6f4eaf837c622b744760508ea239c74e662
Shiv <shivansh.sonit@gmai.com>
2025-11-13 12:10:52 -0500

    Added generate param library

--- snoopy/src/servo_control/launch/launch.py
+++ snoopy/src/servo_control/launch/launch.py
@@ -1,22 +1,39 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode  # <-- correct import in Humble
+from launch_ros.descriptions import ComposableNode
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
+    pkg_name = 'servo_control'
+
+    # Hardcode parameter YAML path
+    params_file = PathJoinSubstitution([
+        FindPackageShare(pkg_name),
+        'config',
+        'servo_control_parameters.yaml'
+    ])
+
+    # Composable node
+    composable_node = ComposableNode(
+        package=pkg_name,
+        plugin='ServoControlLifecycleNode',
+        name='servo_control_node',
+        parameters=[params_file],
+        extra_arguments=[{'use_intra_process_comms': True}]
+    )
+
+    # Container
     container = ComposableNodeContainer(
-        name='servo_control_container',
+        name='servo_container',
         namespace='',
         package='rclcpp_components',
         executable='component_container_mt',
-        output='screen',
-        composable_node_descriptions=[
-            ComposableNode(
-                package='servo_control',
-                plugin='ServoControlLifecycleNode',
-                name='servo_control_lifecycle_node',
-            ),
-        ],
+        composable_node_descriptions=[composable_node],
+        output='screen'
     )
 
-    return LaunchDescription([container])
+    ld = LaunchDescription()
+    ld.add_action(container)
+    return ld
